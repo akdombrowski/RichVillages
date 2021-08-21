6715156627cfba236143ca1a99281564a4e5111a
akdombrowski <akdflyinvee73@gmail.com>
2021-08-21 12:10:44 -0500

    Configuration was backwards.

--- RIchVillagesSettings.cs
+++ RIchVillagesSettings.cs
@@ -18,14 +18,14 @@ namespace RichVillages
     public override string Id => "RichVillages";
 
     [SettingPropertyGroup("{=MCM_001_Settings_Header}General Mod Settings")]
-    [SettingPropertyFloatingInteger("{=MCM_001_Settings_Name_001}Min Village Gold", 1, 10000000, "0 denars", HintText = "{=MCM_001_Settings_Info_001}Controls the min amount of gold (denars) which villages have at the start of the day. (Default = 1k)", RequireRestart = false)]
+    [SettingPropertyFloatingInteger("{=MCM_001_Settings_Name_001}Min Town Gold", 1, 10000000, "0 denars", HintText = "{=MCM_001_Settings_Info_001}Controls the min amount of gold (denars) which towns have at the start of the day. (Default = 1k)", RequireRestart = false)]
     public int MinTownGold
     {
       get; set;
     }
 
     [SettingPropertyGroup("{=MCM_001_Settings_Header}General Mod Settings")]
-    [SettingPropertyFloatingInteger("{=MCM_001_Settings_Name_001}Min Town Gold", 1, 10000000, "0 denars", HintText = "{=MCM_001_Settings_Info_001}Controls the min amount of gold (denars) which towns have at the start of the day. (Default = 100k)", RequireRestart = false)]
+    [SettingPropertyFloatingInteger("{=MCM_001_Settings_Name_001}Min Village Gold", 1, 10000000, "0 denars", HintText = "{=MCM_001_Settings_Info_001}Controls the min amount of gold (denars) which villages have at the start of the day. (Default = 100k)", RequireRestart = false)]
     public int MinVillageGold
     {
       get; set;
