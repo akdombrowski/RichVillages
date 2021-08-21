bb9f747ea5ff302e88e48fce3416950ea8ade57f
akdombrowski <akdflyinvee73@gmail.com>
2021-08-21 12:06:53 -0500

    v1.0.3

--- VillagesPatch.cs
+++ VillagesPatch.cs
@@ -8,35 +8,31 @@ using TaleWorlds.MountAndBlade;
 
 namespace RichVillages
 {
-  [HarmonyPatch(typeof(Village))]
-  public class VillagesPatch : MBSubModuleBase
-  {
-    [HarmonyPatch("DailyTick")]
-    [HarmonyPostfix]
-    public static void DailyTick(ref Town __instance)
+    [HarmonyPatch(typeof(Village))]
+    public class VillagesPatch : MBSubModuleBase
     {
-      Harmony.DEBUG = false;
+        [HarmonyPatch("DailyTick")]
+        [HarmonyPostfix]
+        public static void DailyTick(ref Town __instance)
+        {
+            Harmony.DEBUG = false;
 
-      int maxGold = GlobalSettings<RichVillagesSettings>.Instance.MinVillageGold;
-      FileLog.Log("village maxGold: " + maxGold);
-      Debug.Print("village maxGold: " + maxGold);
+            int maxGold = GlobalSettings<RichVillagesSettings>.Instance.MinVillageGold;
+            FileLog.Log("village maxGold: " + maxGold);
 
-      //InformationManager.DisplayMessage(new InformationMessage("Rich Villages - " + __instance.Name + " - Current Gold: " + __instance.Gold, Color.FromUint(4282569842U)));
+            //InformationManager.DisplayMessage(new InformationMessage("Rich Villages - " + __instance.Name + " - Current Gold: " + __instance.Gold, Color.FromUint(4282569842U)));
 
-      Debug.Print("village __instance.GetName: " + __instance.GetName());
-      Debug.Print("village __instance.Gold: " + __instance.Gold);
-      FileLog.Log("village __instance.GetName: " + __instance.GetName());
-      FileLog.Log("village __instance.Gold: " + __instance.Gold);
+            FileLog.Log("village __instance.GetName: " + __instance.GetName());
+            FileLog.Log("village __instance.Gold: " + __instance.Gold);
 
-      if (__instance.Gold >= maxGold)
-      {
-        return;
-      }
-      __instance.ChangeGold(maxGold - __instance.Gold);
+            if (__instance.Gold >= maxGold)
+            {
+                return;
+            }
+            __instance.ChangeGold(maxGold - __instance.Gold);
 
-      Debug.Print("village __instance.Gold: " + __instance.Gold);
-      FileLog.Log("village __instance.Gold: " + __instance.Gold);
-      Harmony.DEBUG = false;
+            FileLog.Log("village __instance.Gold: " + __instance.Gold);
+            Harmony.DEBUG = false;
+        }
     }
-  }
 }
