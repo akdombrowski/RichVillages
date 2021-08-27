06c5b6d3897698d1c1a905f1785cfd9fc902bd7e
akdombrowski <akdflyinvee73@gmail.com>
2021-08-27 06:35:19 -0500

    v1.0.4 make debugging conditional on debug flag.

--- VillagesPatch.cs
+++ VillagesPatch.cs
@@ -2,6 +2,8 @@
 
 using MCM.Abstractions.Settings.Base.Global;
 
+using System.Collections.Generic;
+
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.Library;
 using TaleWorlds.MountAndBlade;
@@ -16,14 +18,21 @@ namespace RichVillages
         public static void DailyTick(ref Town __instance)
         {
             Harmony.DEBUG = false;
+            List<string> buffer = FileLog.GetBuffer(true);
 
             int maxGold = GlobalSettings<RichVillagesSettings>.Instance.MinVillageGold;
-            FileLog.Log("village maxGold: " + maxGold);
+            if (Harmony.DEBUG)
+            {
+                buffer.Add("village maxGold: " + maxGold);
+            }
 
             //InformationManager.DisplayMessage(new InformationMessage("Rich Villages - " + __instance.Name + " - Current Gold: " + __instance.Gold, Color.FromUint(4282569842U)));
 
-            FileLog.Log("village __instance.GetName: " + __instance.GetName());
-            FileLog.Log("village __instance.Gold: " + __instance.Gold);
+            if (Harmony.DEBUG)
+            {
+                buffer.Add("village __instance.GetName: " + __instance.GetName());
+                buffer.Add("village __instance.Gold: " + __instance.Gold);
+            }
 
             if (__instance.Gold >= maxGold)
             {
@@ -31,7 +40,14 @@ namespace RichVillages
             }
             __instance.ChangeGold(maxGold - __instance.Gold);
 
-            FileLog.Log("village __instance.Gold: " + __instance.Gold);
+            if (Harmony.DEBUG)
+            {
+                buffer.Add("village __instance.Gold: " + __instance.Gold);
+
+                FileLog.LogBuffered(buffer);
+                FileLog.FlushBuffer();
+            }
+
             Harmony.DEBUG = false;
         }
     }
