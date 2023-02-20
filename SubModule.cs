e8b145d51250a55f20ac8a90f5e1b9dad683e9ba
Maker-Macher <63427357+Maker-Macher@users.noreply.github.com>
2023-02-20 23:08:30 +0100

    Properly migrated from MCMv4 to MCMv5 Now saves values changed in MCM Code formatting

--- SubModule.cs
+++ SubModule.cs
@@ -5,12 +5,12 @@ using TaleWorlds.MountAndBlade;
 namespace RichVillages
 {
     public class SubModule : MBSubModuleBase
-  {
-    protected override void OnBeforeInitialModuleScreenSetAsRoot()
     {
-      base.OnBeforeInitialModuleScreenSetAsRoot();
-      new Harmony("com.RichVillages.akdombrowski").PatchAll();
-      InformationManager.DisplayMessage(new InformationMessage("Loaded 'Rich Villages'.", Color.FromUint(4282569842U)));
+        protected override void OnBeforeInitialModuleScreenSetAsRoot()
+        {
+            base.OnBeforeInitialModuleScreenSetAsRoot();
+            new Harmony("com.RichVillages.akdombrowski").PatchAll();
+            InformationManager.DisplayMessage(new InformationMessage("Loaded 'Rich Villages'.", Color.FromUint(4282569842U)));
+        }
     }
-  }
 }
